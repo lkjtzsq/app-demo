4ee09375db4c109d3e1e0603bf3c272dfbeb843f
lkjtzsq <879932397@qq.com>
2018-11-21 00:47:12 +0800

    添加频道页

--- js/load.js
+++ js/load.js
@@ -28,17 +28,20 @@ window.onscroll = function() {
     $('.loading').fadeIn();
     showLoading();
   }
-  st=$('body,html').scrollTop();
-  if(st>300){
+  st = $('body,html').scrollTop();
+  if (st > 300) {
     $('#toolBackTop').show();
-  }else{
+  } else {
     $('#toolBackTop').hide();
   }
 }
 //回到顶部触发事件
-$('#backtotop').click(function(){
-  $('html,body').animate({ scrollTop: 0 }, 300);
+$('#backtotop').click(function() {
+  $('html,body').animate({
+    scrollTop: 0
+  }, 300);
 });
+
 //定时器延迟请求ajax && 显示加载转圈图标
 var showLoading = function() {
   setTimeout(function() {
@@ -49,99 +52,51 @@ var showLoading = function() {
   }, 500)
 }
 var wrapTop = document.getElementById('list-panel')
-
-// 加载4条推荐内容
-var tjNum=0; //初始读取下标
-var tjCount=4; //滚动加载条数
 jQuery.support.cors = true;
-function loadTj(){
-  $.ajax({
-    type: "GET",
-    url: "json/index.json",
-    // async:true,
-    data: {
-
-    },
-    dataType: "json",
-    success: function(data) {
-      var str = '';
-      for (var i = tjNum; i < tjNum+tjCount; i++) {
-        var item = data.data[i];
-        if(item){
-          var imgUrl = item.imgs[0];
-          var title = item.title;
-          var id = item.id;
-          var url=item.newsurl;
-          var paras='';
-          var newUrl=url.split('cmsfile')[1].split('/');
-          for(var k=1;k<newUrl.length-1;k++){
-            var para='para'+k;
-            paras+=('&'+para+'='+newUrl[k]);
-          }
-          var urlId=newUrl[newUrl.length-1].split('.html')[0];
-          paras+=('&urlId='+urlId);
-          paras=paras.slice(1,paras.length);
-          paras='iframe.html?'+paras;
-          var desc=item.outline;
-          var viewcount=item.viewcount;
-          var update_time=item.update_time;
-          var copyfrom=item.copyfrom;
-
-          str += '<div class="news-li"><div class="news-pic"><a href=' + paras + ' target="_blank"><img src=' + imgUrl + '></a></div><div class="h2p"><h2><a href=' + paras + ' target="_blank">'+title+'</a></h2><p>' + desc + '</p></div><div class="pdtt_trbs">' + copyfrom + '<span>' + update_time + '</span><div class="trbstxt">推荐</div></div></div>';
-        }
-
-      }
-      tjNum+=tjCount;
-      $(str).appendTo($('#list-panel'));
-    }
-  });
-}
 // 无限加载普通内容
-var ptNum=0; //初始读取下标
-var ptCount=10; //滚动加载条数
-function loadPt(){
+var ptNum = 0; //初始读取下标
+var ptCount = 10; //滚动加载条数
+function loadPt() {
   $.ajax({
     type: "GET",
     url: "json/index.json",
     data: {
 
     },
-    contentType:"text/plain",
+    contentType: "text/plain",
     dataType: "json",
     success: function(data) {
       var str = '';
-      for (var i = ptNum; i < ptNum+ptCount; i++) {
+      for (var i = ptNum; i < ptNum + ptCount; i++) {
         var item = data.data[i];
-        if(item){
+        if (item) {
           var imgUrl = item.imgs[0];
           var title = item.title;
           var id = item.id;
-          var url=item.newsurl;
-          var paras='';
-          var newUrl=url.split('cmsfile')[1].split('/');
-          for(var k=1;k<newUrl.length-1;k++){
-            var para='para'+k;
-            paras+=('&'+para+'='+newUrl[k]);
+          var url = item.newsurl;
+          var paras = '';
+          var newUrl = url.split('cmsfile')[1].split('/');
+          for (var k = 1; k < newUrl.length - 1; k++) {
+            var para = 'para' + k;
+            paras += ('&' + para + '=' + newUrl[k]);
           }
-          var urlId=newUrl[newUrl.length-1].split('.html')[0];
-          paras+=('&urlId='+urlId);
-          paras=paras.slice(1,paras.length);
-          paras='iframe.html?'+paras;
-          var desc=item.outline;
-          var viewcount=item.viewcount;
-          var update_time=item.update_time;
-          var copyfrom=item.copyfrom;
+          var urlId = newUrl[newUrl.length - 1].split('.html')[0];
+          paras += ('&urlId=' + urlId);
+          paras = paras.slice(1, paras.length);
+          paras = 'iframe.html?' + paras;
+          var desc = item.outline;
+          var viewcount = item.viewcount;
+          var update_time = item.update_time;
+          var copyfrom = item.copyfrom;
 
-          str += '<div class="news-li"><div class="news-pic"><a href=' + paras + ' target="_blank"><img src=' + imgUrl + '></a></div><div class="h2p"><h2><a href=' + paras + ' target="_blank">'+title+'</a></h2><p>' + desc + '</p></div><div class="pdtt_trbs">' + copyfrom + '<span>' + update_time + '</span></div></div>';
+          str += '<div class="news-li"><div class="news-pic"><a href=' + paras + ' target="_blank"><img src=' + imgUrl + '></a></div><div class="h2p"><h2><a href=' + paras + ' target="_blank">' + title + '</a></h2><p>' + desc + '</p></div><div class="pdtt_trbs">' + '<a href="" target="_blank">'+ copyfrom + '</a><span>' + update_time + '</span></div></div>';
         }
 
       }
-      ptNum+=ptCount;
+      ptNum += ptCount;
       $(str).appendTo($('#list-panel'));
     }
   });
 }
-//初始加载4条推荐新闻
-loadTj();
 //无限加载普通新闻
 loadPt();
